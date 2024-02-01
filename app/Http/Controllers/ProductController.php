39d0a38cb688828ef7400c13da3dc6319b1ab839
Uttam-Sarkar <s2010876149ac.ru.bd>
2024-02-02 00:09:53 +0600

    make my 1st committe after create a laravel apps

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function index(){
+        return view('products.index');
+    }
+
+    public function create(){
+        return view('products.create');
+    }
+
+    public function store(Request $request){
+        dd($request);
+    }
+}
