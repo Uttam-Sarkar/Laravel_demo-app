258226bb84413c23f4c6ff959739d97d7216e610
Uttam-Sarkar <s2010876149ac.ru.bd>
2024-02-02 10:55:15 +0600

    Add update fuction

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -9,7 +9,9 @@ use App\Models\Product;
 class ProductController extends Controller
 {
     public function index(){
-        return view('products.index');
+
+        $products = Product::all();
+        return view('products.index', ['products' => $products] );
     }
 
     public function create(){
@@ -27,5 +29,22 @@ class ProductController extends Controller
         $newProduct = Product::create($data);
 
         return redirect(route('product.index'));
+    }
+
+    public function edit(Product $product){
+        //dd($product);
+        return view('products.edit', ['product' => $product]);
+
+    }
+    public function update(Product $product, Request $request){
+        $data = $request->validate([
+            'name' => 'required',
+            'qty' => 'required|numeric',
+            'price' => 'required|decimal:0,2',
+            'description' => 'nullable',
+        ]);
+        $product->update($data);
+
+        return redirect(route('product.index'))->with('success','Product Updated Succesffully');
     }
 }
